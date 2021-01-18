8c7609b684dad44927646111aa2e89b959e0b1f2
Gelo-V <30360628+Gelo-V@users.noreply.github.com>
2021-01-18 16:38:15 -0600

    Add project files.

---
+++ TinifyTest/TinifyTest/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using TinifyAPI;
+using Xamarin.Forms;
+
+namespace TinifyTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    Tinify.Key = "PUT_AN_API_KEY_HERE";
+                    bool validKey = await Tinify.Validate();
+                    Debug.WriteLine($"[TINIFY]: Was conexion valid?: {validKey}");
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+            });
+        }
+    }
+}
